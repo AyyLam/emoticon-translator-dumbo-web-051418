68540a7836ef7db39c71bfcde8484cfd02dae537
Anthony Lam <al3101@nyu.edu>
2018-05-10 19:59:36 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,27 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  file = YAML.load_file(path)
+  new_file = {}
+  get_meaning = {}
+  get_emoticon = {}
+  
+  file.each do |emotion, values|
+    get_meaning[values[1]] = emotion
+    get_emoticon[values[0]] = values[1] 
+  end
+  new_file["get_meaning"] = get_meaning
+  new_file["get_emoticon"] = get_emoticon
+  new_file
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  file = load_library(path)
+  file[get_emoticon].each do |key, value|
+    if key == emoticon
+      return value
+    end
+  end
 end
 
 def get_english_meaning
